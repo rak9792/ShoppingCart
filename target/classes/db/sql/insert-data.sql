a61cbec7c9c5a9bb6cc7cc429d1edbc090c82c4c
rakshit <rv09071992@gmail.com>
2018-02-06 22:07:36 +0530

    final commit for 6th feb 2018

---
+++ target/classes/db/sql/insert-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO PRODUCTS VALUES('P1234','iPhone 6s','Apple smartphone with 4.00-inch display and 8-megapixel rear camera',500,'Apple','SmartPhone','New',450,0,false);
+INSERT INTO PRODUCTS VALUES('P1235','Dell Inspiron','Dell Inspiron 14 inch laptop',700,'Dell','Laptop','New',1000,0,false);
+INSERT INTO PRODUCTS VALUES('P1236','Nexus 7','Google Nexus 7 tablet',300,'Google','Tablet','New',1000,0,false);
